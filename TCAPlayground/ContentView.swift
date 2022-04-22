cbd02a31450be790c0cde9d169f4f2c17c52a6e2
jsryudev <jsryu.dev@gmail.com>
2022-04-23 02:29:03 +0900

    chore: init project

---
+++ TCAPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TCAPlayground
+//
+//  Created by Junsang Ryu on 2022/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ TCAPlayground/TCAPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAPlaygroundApp.swift
+//  TCAPlayground
+//
+//  Created by Junsang Ryu on 2022/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TCAPlaygroundApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
